5775eafc1c6347ed2ffc160bdaf40bc1ef9d01ab
NaifMohamed1998 <107547726+NaifMohamed1998@users.noreply.github.com>
2022-12-14 15:07:50 +0530

    front end without task widget and registered worker screen

---
+++ workos/lib/constants/constant.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+
+class Constants {
+  static Color darkBlue = Color(0xff00325A);
+  static List<String> taskCategoryList = [
+    "Bussiness",
+    "Programming",
+    "Information Technology",
+    "Human Resource",
+    "Marketing",
+    "Design",
+    "Accounting"
+  ];
+  static List<String> jobList = [
+    "Manager",
+    "Team Leader",
+    "Designer",
+    "Web designer",
+    "Full Stack Developer",
+    "Mobile Developer",
+    "Marketing",
+    "Digital Marketing"
+  ];
+}
